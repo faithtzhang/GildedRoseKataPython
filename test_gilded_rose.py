e500e57666ff3ff9d904573a0d5a7034d7907103
faithzhang <tianxinfaith720@gmail.com>
2024-10-01 21:17:21 -0700

    lab4

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -11,6 +11,27 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
 
+    def test_invalid_quality(self):
+        items = [Item("item1", 0, 60)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+
+        self.assertEqual(60, items[0].quality) # invalid number > 50
+
+    def test_sell_in_sulfuras(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 1, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+
+        self.assertEqual(0, items[0].sell_in) # expects sell_in 1
+
+    def test_quality_aged_brie(self):
+        items = [Item("Aged Brie", 2, 1)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+
+        self.assertEqual(1, items[0].quality) # expects quality 2
+
 
 if __name__ == '__main__':
     unittest.main()
