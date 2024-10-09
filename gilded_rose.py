86b4681d6fbc458eaa3a0a4056b8dff3d7cc7dc1
faithzhang <tianxinfaith720@gmail.com>
2024-10-09 15:49:46 -0700

    lab 5 - tests passed

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
+from abc import abstractmethod, ABC
+
+
+# Helper function to check for invalid quality
+def validate_quality(item):
+    if item.quality > 50 or item.quality < 0:
+        raise ValueError(f"Invalid quality for item {item.name}: {item.quality}")
 
 
 class Item:
     """ DO NOT CHANGE THIS CLASS!!!"""
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
@@ -12,38 +20,72 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
-class GildedRose(object):
+# Use strategy pattern to update quality
+class ItemUpdateStrategy(ABC):
+    @abstractmethod
+    def update_quality(self, item: "Item"):
+        pass
 
+
+# update for normal items
+class NormalItemUpdate(ItemUpdateStrategy):
+    def update_quality(self, item: Item):
+        if item.quality > 0:
+            item.quality -= 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality > 0:  # past sell in, degrade twice as fast.
+            item.quality -= 1
+        validate_quality(item)
+
+
+# update for "Aged Brie"
+class AgedBrieUpdate(ItemUpdateStrategy):
+    def update_quality(self, item: Item):
+        if item.quality < 50:
+            item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality += 1
+        validate_quality(item)
+
+
+# update for "Backstage passes to a TAFKAL80ETC concert"
+class BackstagePassesUpdate(ItemUpdateStrategy):
+    def update_quality(self, item: Item):
+        if item.quality < 50:
+            item.quality += 1
+            if item.sell_in < 11:
+                item.quality += 1
+            if item.sell_in < 6:
+                item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality = 0
+        validate_quality(item)
+
+
+# update for "Sulfuras, Hand of Ragnaros"
+class SulfurasUpdate(ItemUpdateStrategy):
+    def update_quality(self, item: Item):
+        validate_quality(item)
+        pass  # Sulfuras does not change
+
+
+class GildedRose(object):
     def __init__(self, items: list[Item]):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
 
+        self.updateMethods = {
+            "Aged Brie": AgedBrieUpdate(),
+            "Backstage passes to a TAFKAL80ETC concert": BackstagePassesUpdate(),
+            "Sulfuras, Hand of Ragnaros": SulfurasUpdate(),
+        }
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            try:
+                updater = self.updateMethods.get(item.name, NormalItemUpdate())
+                updater.update_quality(item)
+            except ValueError as e:
+                print(f"Error updating quality for {item.name}: {e}")
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -4,32 +4,26 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
-
     def test_invalid_quality(self):
-        items = [Item("item1", 0, 60)]
+        items = [Item("item1", 10, 40)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
 
-        self.assertEqual(60, items[0].quality) # invalid number > 50
+        self.assertEqual(39, items[0].quality) # quality decreases by 1
 
     def test_sell_in_sulfuras(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", 1, 0)]
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
 
-        self.assertEqual(0, items[0].sell_in) # expects sell_in 1
+        self.assertEqual(20, items[0].quality) # quality does not change
 
     def test_quality_aged_brie(self):
-        items = [Item("Aged Brie", 2, 1)]
+        items = [Item("Aged Brie", 0, 40)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
 
-        self.assertEqual(1, items[0].quality) # expects quality 2
+        self.assertEqual(42, items[0].quality) # quality increases by 2
 
 
 if __name__ == '__main__':
